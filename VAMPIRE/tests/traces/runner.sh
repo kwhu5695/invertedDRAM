c76011b31460cbbdafd292be96f6ae5e384d6895
Daniel Ao <dao3@illinois.edu>
2024-04-10 16:41:04 -0500

    csv generation

--- VAMPIRE/tests/traces/runner.sh
+++ VAMPIRE/tests/traces/runner.sh
@@ -6,8 +6,8 @@ for ((N=1; N<=128; N++)); do
     filename="${N}-wr.trace"
 
     # Construct the output filename
-    output_filename="outputCSVs/Fs/${N}-wr.csv"
+    output_filename="${N}-wr.csv"
 
     # Run the command on the file
-    ./vampire -f "modified_files/${filename}" -c configs/default.cfg -d WR -p ASCII -csv "$output_filename"
+    ../../vampire -f "modified_files/${filename}" -c ../../configs/default.cfg -d WR -p ASCII -csv "csvs/$output_filename"
 done
